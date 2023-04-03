291f74fbb6272fe1c3fc303a63fbbb32185045f0
kai <khaipv@ints.vn>
2023-04-03 17:34:15 +0700

    hihi

--- src/routes/friendRoute.js
+++ src/routes/friendRoute.js
@@ -4,5 +4,6 @@ const friendController = require("../controllers/friendController");
 const friendRoute = express.Router();
 
 friendRoute.get("/", verifyToken, friendController.getInfoCommunication);
+friendRoute.post("/add", friendController.addFriend);
 
 module.exports = friendRoute;
--- src/routes/servicesRoute.js
+++ src/routes/servicesRoute.js
@@ -4,5 +4,6 @@ const servicesController = require("../controllers/servicesController");
 const servicesRoute = express.Router();
 
 servicesRoute.get("/users", servicesController.getUsers);
+servicesRoute.get("/search", servicesController.searchUser);
 
 module.exports = servicesRoute;
