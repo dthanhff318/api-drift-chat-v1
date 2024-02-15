98c315f01518c33bf2f44f72fb4ceebe7060a563
dthanhff <dthanhff318@gmail.com>
2024-02-15 17:28:46 +0700

    update code

--- src/controllers/historyProfile.controller.js
+++ src/controllers/historyProfile.controller.js
@@ -6,7 +6,8 @@ const historyProfileController = {
   getHistoryByUserId: async (req, res) => {
     try {
       const { id } = req.infoUser;
-      return await historyProfileServices.getHistoryByOwnerId(id);
+      const histories = await historyProfileServices.getHistoryByOwnerId(id);
+      return res.status(httpStatus.OK).json(histories);
     } catch (err) {
       console.log(err);
       return res.status(httpStatus.INTERNAL_SERVER_ERROR).json(err);
